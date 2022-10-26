1a9b01a2179ae603b665ad3b3b068707e6a70fb8
LorenzoMoccia <moccialorenzo98@gmail.com>
2022-10-26 12:33:50 +0200

    Oh yeeeees

---
+++ jsnack5/js/main.js
@@ -0,0 +1,17 @@
+"use strict";
+
+ 
+let numberArray = [];
+
+for(let i = 0; i < 6; i++){
+    const number = prompt("Inserisci un numero");
+
+    if(number % 2 === 0){
+        
+    }
+    else{
+    numberArray.push(number);
+    }
+}
+
+console.log(numberArray);
