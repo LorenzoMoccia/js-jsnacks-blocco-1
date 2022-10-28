c5e0735ca94dcb4955b92b3696b474b916471a08
LorenzoMoccia <moccialorenzo98@gmail.com>
2022-10-28 10:32:39 +0200

    Finito

---
+++ jsnack6/js/main.js
@@ -0,0 +1,23 @@
+"use strict";
+
+ 
+let numberArray = [1, 10, 15, 90, 73, 23];
+let oddNumberArray = [];
+let i = 0;
+
+while(i < numberArray.length){
+    if(numberArray[i] % 2 !== 0){
+        const oddNumber = numberArray[i];
+        oddNumberArray.push(oddNumber);
+    }
+    
+    i++;
+}
+
+let sum = 0;
+
+for(let i = 0; i < oddNumberArray.length; i++){
+    sum += oddNumberArray[i];
+}
+
+console.log(sum);
